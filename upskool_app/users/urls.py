e68659be9b20d4317e920a5ba7dd0e249095cd4c
akankshasrivastava-98 <akankshabsivastava@gmail.com>
2021-01-13 22:26:20 +0530

    profile update

--- upskool_app/users/urls.py
+++ upskool_app/users/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import (
+    ReqListView,
+    ReqDetailView,
+    ReqCreateView,
+    ReqUpdateView,
+    ReqDeleteView
+)
+
 
 urlpatterns = [
     path('register/', views.register, name='upskool-register'),
+    path('', views.home, name='upskool-home'),
+    path('profile/',views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('requirements/',ReqListView.as_view(), name='requirements'),
+    path('stories/', views.stories, name='stories'),
+    path('requirements/<int:pk>/',ReqDetailView.as_view() , name='req-detail'),
+    path('requirements/new/', ReqCreateView.as_view(), name='req-create'),
+    path('requirements/<int:pk>/update/', ReqUpdateView.as_view(), name='req-update'),
+    path('requirements/<int:pk>/delete/', ReqDeleteView.as_view(), name='req-delete'),
 ]
 
--- upskool_app/users/views.py
+++ upskool_app/users/views.py
@@ -1,8 +1,67 @@
   
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.views.generic import ListView, DetailView
+from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
+
+def home(request):
+    return render(request, 'users/home.html')
+
+class ReqListView(ListView):
+    model = Requirement
+    template_name = 'users/requirements.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'requirements'
+    ordering = ['-date_posted']
+
+class ReqDetailView(DetailView):
+    model = Requirement
+
+class ReqCreateView(LoginRequiredMixin, CreateView):
+    model = Requirement
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ReqUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Requirement
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        req = self.get_object()
+        if self.request.user == req.author:
+            return True
+        return False
+
+class ReqDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Requirement
+    success_url = '/'
+
+    def test_func(self):
+        req = self.get_object()
+        if self.request.user == req.author:
+            return True
+        return False
+
+
+def stories(request):
+    return render(request, 'users/stories.html')
 
 def register(request):
     if request.method == 'POST':
@@ -11,7 +70,32 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('upskool-register')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
